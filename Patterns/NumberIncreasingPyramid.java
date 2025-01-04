946302367ebba6beceab4a1914cbbe1a49e1767d
Shubham Maurya <160427804+shubham-maurya-sm@users.noreply.github.com>
2025-01-04 23:22:12 +0530

    program Number Increasing Pyramid added
        1
        1 2
        1 2 3
        1 2 3 4 
        1 2 3 4 5
        1 2 3 4 5 6

---
+++ Patterns/NumberIncreasingPyramid.java
@@ -0,0 +1,40 @@
+/*  Que- Number Increasing Pyramid
+  
+    1
+    1 2
+    1 2 3
+    1 2 3 4 
+    1 2 3 4 5
+    1 2 3 4 5 6
+  
+ */
+
+
+package Patterns;
+
+import java.util.Scanner;
+
+public class NumberIncreasingPyramid {
+    
+    public void NumberPyramid(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter the value of n : ");
+        int n=sc.nextInt();
+        System.out.println("\n");
+
+        NumberIncreasingPyramid obj=new NumberIncreasingPyramid();
+
+        obj.NumberPyramid(n);
+    }
+}
